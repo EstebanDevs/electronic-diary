345c529a34a59cf18d1e35a876298700980cd7d1
Esteban Perafan <bestebanperafan@gmail.com>
2023-10-26 22:42:01 -0300

    Finish electronic diary project in Java with Maven

---
+++ src/main/java/Logic/ElectronicDiary.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logic;
+import GUI.ElectronicDiaryView;
+/**
+ *
+ * @author Esteban
+ */
+public class ElectronicDiary {
+
+    public static void main(String[] args) {
+        ElectronicDiaryView view = new ElectronicDiaryView();
+        view.setVisible(true);
+        view.setLocationRelativeTo(null);
+        
+
+    }
+}
